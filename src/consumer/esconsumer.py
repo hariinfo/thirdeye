a7a840b6071fccf5d2094e95fed86d7cabce569a
hariinfo <hariinfo@gmail.com>
2020-03-16 09:41:45 -0500

    added kafka consumer

--- src/consumer/esconsumer.py
+++ src/consumer/esconsumer.py
@@ -9,26 +9,36 @@ Created on Mon Mar 16 07:46:49 2020
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 from confluent_kafka import Consumer
-
+import json
 
 running = True
 #Consume Kafka message
 conf = {
                 'bootstrap.servers':'192.168.56.101:9092',
-                'queue.buffering.max.messages': 500000,
-                'queue.buffering.max.ms': 60000,
-                'batch.num.messages': 100,
-                'log.connection.close': False,
-                'group.id': "es-group",
-                'default.topic.config': {'acks': 'all'}
+                'group.id': "esgroup",
+                'auto.offset.reset': 'earliest',
+                'enable.auto.commit': False
             }
-def basic_consume_loop(consumer, topics):
+MIN_COMMIT_COUNT = 1000;
+
+def process(es, record):
+   print("Indexing records")
+   res = es.index(index="my-index", body=record.value().decode('utf-8'))
+   print(res['result'])
+
+    
+def basic_consume_loop(consumer, topics, es):
     try:
+        msg_count = 0
         consumer.subscribe(topics)
         print("Subscribed to {}...".format(topics))
         while running:
+            print("Polling...")
             msg = consumer.poll(timeout=1.0)
-            if msg is None: continue
+            if msg is None:
+                continue
+            
+                
 
             if msg.error():
                 print("Error Process Message...")
@@ -40,7 +50,11 @@ def basic_consume_loop(consumer, topics):
                     raise KafkaException(msg.error())
             else:
                 print("Process Message...")
-                print(msg)
+                process(es, msg)
+                msg_count += 1
+                print("Message count {}".format(msg_count))
+                if msg_count % MIN_COMMIT_COUNT == 0:
+                    consumer.commit(asynchronous=False)
     finally:
         print("Closing...")
         # Close down consumer to commit final offsets.
@@ -50,7 +64,8 @@ def basic_consume_loop(consumer, topics):
 def main():
     # Apache Kafka connection
     consumer = Consumer(conf)
-    basic_consume_loop(consumer, ["thirdeye_raw"])
+    es = Elasticsearch()
+    basic_consume_loop(consumer, ["thirdeye_raw"], es)
 
 
 def shutdown():
